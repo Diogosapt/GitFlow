91486bc4376c7aa9563a487fdc8a8ea395d92ef2
Diogo Sá <diogo.pinto@decode.pt>
2022-12-09 14:45:10 +0000

    Add Controller and view

--- GitFlow/Controllers/EmployeeController.cs
+++ GitFlow/Controllers/EmployeeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace GitFlow.Controllers
 {
-    public class HomeController : Controller
+    public class EmployeeController : Controller
     {
         public ActionResult Index()
         {
