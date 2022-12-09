fe31aa7e37a1b573ba5d94d27f570e88904c491d
Diogo Sá <diogo.pinto@decode.pt>
2022-12-09 12:14:43 +0000

    Add model

---
+++ GitFlow/Repository/DataRepository.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using GitFlow.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GitFlow.Repository
+{
+    public class DataRepository
+    {
+        public SqlConnection conn;
+
+        private void Connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["myConnectionString"].ToString();
+            conn = new SqlConnection(constr);
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            try
+            {
+                Connection();
+                conn.Open();
+                conn.Execute("AddEmployee", employee, commandType: CommandType.StoredProcedure);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
